30b735bd01837bc7fbcc2cd4e159e4f004288609
Amir Mishayev <127884315+amir3x0@users.noreply.github.com>
2025-05-12 11:30:34 +0300

    updated google services

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.children_book_app"
+    namespace = "com.example2.children_book_app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -22,7 +22,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.children_book_app"
+        applicationId = "com.example2.children_book_app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
